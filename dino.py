cbda22549bcd9c36043125cf7678197ae6f6ffdf
Kendall Morgan <kuyukip@gmail.com>
2022-04-23 11:12:56 -0500

    forgot the dino class oops. Created that and modified weapon class

---
+++ dino.py
@@ -0,0 +1,11 @@
+
+class Dinosaur:
+
+    def __init__(self, name, attack_power, health):
+        self.name = name
+        self.attack_power = attack_power
+        self.health = health
+
+
+    def attack(self, robot):
+        pass
