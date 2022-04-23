609e83e6d51940171e6db4f6c95dc0cb8249b37b
Kendall Morgan <kuyukip@gmail.com>
2022-04-23 11:18:25 -0500

    created fleet and herd classes properly...very similar

---
+++ fleet.py
@@ -0,0 +1,12 @@
+
+from robot import Robot
+
+class Fleet:
+
+    def __init__(self):
+        self.robot = Robot([])
+
+    def create_fleet(self):
+        pass
+
+
---
+++ herd.py
@@ -0,0 +1,10 @@
+
+from dino import Dinosaur
+
+class Herd:
+
+    def __init__(self):
+        self.dino = Dinosaur([])
+
+    def create_herd(self):
+        pass
